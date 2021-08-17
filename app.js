3bd45807af125b8786f043c6d715044fb1c9fc18
Taj Crooms <tajedwardc@gmail.com>
2021-08-17 13:18:37 -0400

    Pseudocode commit 1

---
+++ app.js
@@ -0,0 +1,5 @@
+// Heroes vs. Villains
+
+// game starts with you picking heroes or villains.  once a side is chosen the each caster (player one and computer) is assigned 20 life points.  
+
+// you select your 
