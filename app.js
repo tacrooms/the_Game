4ffd7910d16ee0db59e839033a8d5a2ac09264bc
Taj Crooms <tajedwardc@gmail.com>
2021-08-18 10:15:48 -0400

    Hero and Demon CLasses

--- app.js
+++ app.js
@@ -1,5 +1,31 @@
 // Heroes vs. Villains
 
-// game starts with you picking heroes or villains.  once a side is chosen the each caster (player one and computer) is assigned 20 life points.  
+// game starts with you picking heroes or villains.  once a side is chosen the each caster (player one and computer) is assigned 20 life points.
+
+// if heros hp goes to zero they are knocked down and cannot attack for three turns. after 3 turns their HP is restored 
+
 
 // you select your 
+
+class hero {
+    constructor(name, attack, speed, health, heroClass, specialAbility){
+        this.name = name;
+        this.attack = attack;
+        this.speed = speed;
+        this.health = health;
+        this.heroClass = heroClass;
+        this.specialAbility = specialAbility;
+
+    }
+}
+
+class demon {
+    constructor(name, attack, speed, health, specialAbility){
+        this.name = name;
+        this.attack = attack;
+        this.speed = speed;
+        this.health = health;
+        this.specialAbility = specialAbility;
+
+    }
+}
